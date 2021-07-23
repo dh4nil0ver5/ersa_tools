9a177f65f808784c08ad7f0f010b98271d5c8d99
unknown <dh4nil0ver5>
2021-07-23 21:38:15 +0700

    add new files

---
+++ lib/bloc/navigation_bloc/navigation_bloc.dart
@@ -0,0 +1,44 @@
+import 'package:bloc/bloc.dart';
+import 'package:ersatools_new/ui/pages/messages_page.dart';
+import 'package:ersatools_new/ui/pages/my_cards_page.dart';
+import 'package:ersatools_new/ui/pages/utility_bills_page.dart';
+
+enum NavigationEvents {
+  DashboardClickedEvent,
+  MessagesClickedEvent,
+  UtilityClickedEvent
+}
+
+abstract class NavigationStates {}
+
+class NavigationBloc extends Bloc<NavigationEvents, NavigationStates> {
+  final Function onMenuTap;
+
+  NavigationBloc({this.onMenuTap});
+
+  @override
+  NavigationStates get initialState => MyCardsPage(
+        onMenuTap: onMenuTap,
+      );
+
+  @override
+  Stream<NavigationStates> mapEventToState(NavigationEvents event) async* {
+    switch (event) {
+      case NavigationEvents.DashboardClickedEvent:
+        yield MyCardsPage(
+          onMenuTap: onMenuTap,
+        );
+        break;
+      case NavigationEvents.MessagesClickedEvent:
+        yield MessagesPage(
+          onMenuTap: onMenuTap,
+        );
+        break;
+      case NavigationEvents.UtilityClickedEvent:
+        yield UtilityBillsPage(
+          onMenuTap: onMenuTap,
+        );
+        break;
+    }
+  }
+}
